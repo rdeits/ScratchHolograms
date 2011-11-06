4f96ae00f20948c284b9f8408cdfc2ec816c4d23
Robin Deits <robin.deits@gmail.com>
2011-11-06 14:22:58 -0500

    "SolidPatternMaker" has now obsoleted and replaced "PatternMaker" as default

--- draw_pattern.py
+++ draw_pattern.py
@@ -1,14 +1,14 @@
 from __future__ import division
 print "1"
-from pattern import PatternMaker, SolidPatternMaker, GridPatternMaker
+from pattern import PatternMaker, GridPatternMaker
 from printer import PDFPrinter, DXFPrinter
 import sys
 import numpy as np
 print "2"
 
 filename = sys.argv[1]
-spat = SolidPatternMaker(filename, PDFPrinter())
-spat.print_pattern()
-spat.draw_view(-25*np.pi/180, '_left')
-spat.draw_view(25*np.pi/180, '_right')
+pat = PatternMaker(filename, PDFPrinter())
+pat.print_pattern()
+pat.draw_view(-25*np.pi/180, '_left')
+pat.draw_view(25*np.pi/180, '_right')
 print "done"
--- pattern.py
+++ pattern.py
@@ -13,23 +13,14 @@ __author__ = "Robin Deits <robin.deits@gmail.com>"
 RESOLUTION = 60
 
 class PatternMaker:
-    def __init__(self, reader, printer, image_width_in = 4):
-        self.setup_common(reader, printer, image_width_in)
-
-    def setup_common(self, reader, printer, image_width_in):
-        self.reader = reader
-        self.data = self.reader.to_array()
+    def __init__(self, filename, printer, image_width_in = 4):
+        self.filename = filename
+        self.reader = csv.reader(open(filename, 'rb'))
         self.printer = printer
+        self.data = np.array([[float(i) for i in row] for row in self.reader])
+        print self.data
+        print np.abs(self.data)
         self.rescale(image_width_in)
-        self.filename = self.reader.filename
-        z_max = np.max(np.abs(self.data[:,2]))
-        self.x_range = (np.max(self.data[:,0]) 
-                - np.min(self.data[:,0]) 
-                + 2*z_max)
-        self.y_range = (np.max(self.data[:,1]) 
-                - np.min(self.data[:,1])
-                + 2*z_max)
-        self.overall_range = max(self.x_range, self.y_range)
 
     def rescale(self, image_width_in):
         z_max = np.max(np.abs(self.data[:,2]))
@@ -40,41 +31,33 @@ class PatternMaker:
 
     def print_pattern(self):
         num_points = len(self.data[:,0])
-        printed = np.zeros(num_points)
-        min_dist = self.overall_range / RESOLUTION
         for i in range(num_points):
-            too_close = False
-            for j in range(i):
-                if printed[j] and distance(self.data[i,:], self.data[j,:]) < min_dist:
-                    too_close = True
-                    # print "skipped"
-                    break
-            if too_close:
-                continue
-            else:
-                # print "printing"
-                printed[i] = 1
-                self.plot_point(self.data[i, 0], self.data[i, 1], self.data[i, 2])
+            self.plot_point(self.data[i,:])
         self.printer.save(os.path.splitext(self.filename)[0])
 
-    def plot_point(self, x, y, z):
-        if z < 0:
-            color = 'k'
-        else:
-            color = 'k'
-        self.printer.draw_arc(center = [x, y+z], r = -z, color = color)
+    def plot_point(self, point):
+        x = point[0]
+        y = point[1]
+        z = point[2]
+        angles = -np.array([point[3], point[4]]) + np.pi / 2
+        self.printer.draw_arc([y, z + x], -x,
+                              angles = angles, color='k')
+        # self.printer.draw_arc([y, z+x], -x,
+        #                       angles = [angle - angle_delta, angle + angle_delta])
 
     def draw_view(self, angle, name=''):
-        view_printer = PDFPrinter()
         num_points = len(self.data[:,0])
+        view_printer = PDFPrinter()
         for i in range(num_points):
             x = self.data[i,0]
             y = self.data[i,1]
             z = self.data[i,2]
-            self.plot_point(x, y, z)
-            plt.plot(x - z*np.cos(angle+np.pi/2), y + z - z*np.sin(angle+np.pi/2),
-                     'k*')
-        self.printer.save(os.path.splitext(self.filename)[0] + name)
+            if self.data[i, 3] < angle < self.data[i,4]:
+                draw_angle = -angle + np.pi/2
+                view_printer.draw_point([y - x * np.cos(draw_angle),
+                         z + x - x * np.sin(draw_angle)], marker = '*', 
+                                        color = 'k')
+        view_printer.save(os.path.splitext(self.filename)[0] +name)
 
     def draw_views(self, angle):
         if isinstance(self.printer, DXFPrinter):
@@ -100,6 +83,21 @@ class GridPatternMaker(PatternMaker):
         self.bin_angles = np.zeros((len(self.x_bins), len(self.y_bins))) + np.pi/2
         for i in range(len(self.data[:,0])):
             self.plot_point(self.data[i, 0], self.data[i, 1], self.data[i, 2])
+
+    def setup_common(self, reader, printer, image_width_in):
+        self.reader = reader
+        self.data = self.reader.to_array()
+        self.printer = printer
+        self.rescale(image_width_in)
+        self.filename = self.reader.filename
+        z_max = np.max(np.abs(self.data[:,2]))
+        self.x_range = (np.max(self.data[:,0]) 
+                - np.min(self.data[:,0]) 
+                + 2*z_max)
+        self.y_range = (np.max(self.data[:,1]) 
+                - np.min(self.data[:,1])
+                + 2*z_max)
+        self.overall_range = max(self.x_range, self.y_range)
 
     def print_pattern(self):
         r = self.bin_width / 2
@@ -147,44 +145,3 @@ class GridPatternMaker(PatternMaker):
 
 def distance(p0, p1):
     return np.sqrt(np.sum(np.power(np.array(p1) - np.array(p0), 2)))
-
-class SolidPatternMaker(PatternMaker):
-    def __init__(self, filename, printer, image_width_in = 4):
-        self.filename = filename
-        self.reader = csv.reader(open(filename, 'rb'))
-        self.printer = printer
-        self.data = np.array([[float(i) for i in row] for row in self.reader])
-        print self.data
-        print np.abs(self.data)
-        # self.rescale(image_width_in)
-
-    def print_pattern(self):
-        num_points = len(self.data[:,0])
-        for i in range(num_points):
-            self.plot_point(self.data[i,:])
-        self.printer.save(os.path.splitext(self.filename)[0])
-
-    def plot_point(self, point):
-        x = point[0]
-        y = point[1]
-        z = point[2]
-        angles = -np.array([point[3], point[4]]) + np.pi / 2
-        self.printer.draw_arc([y, z + x], -x,
-                              angles = angles, color='k')
-        # self.printer.draw_arc([y, z+x], -x,
-        #                       angles = [angle - angle_delta, angle + angle_delta])
-
-    def draw_view(self, angle, name=''):
-        num_points = len(self.data[:,0])
-        view_printer = PDFPrinter()
-        for i in range(num_points):
-            x = self.data[i,0]
-            y = self.data[i,1]
-            z = self.data[i,2]
-            if self.data[i, 3] < angle < self.data[i,4]:
-                draw_angle = -angle + np.pi/2
-                view_printer.draw_point([y - x * np.cos(draw_angle),
-                         z + x - x * np.sin(draw_angle)], marker = '*', 
-                                        color = 'k')
-        view_printer.save(os.path.splitext(self.filename)[0] +name)
-
